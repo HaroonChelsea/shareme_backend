8ff3c60af66f081d447f24a4189d4f09cf1bb099
Meistaaa <alimuhammad9868@gmail.com>
2022-12-21 02:06:16 +0500

    sanity

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import comment from './comment'
+import pin from './pin'
+import user from './user'
+import save from './save'
+import postedBy from './postedBy'
+export const schemaTypes = [user,postedBy,pin,comment,save ]
+
